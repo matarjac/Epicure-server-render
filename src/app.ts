d0fb331db1fe09c5649d139e043c0c84b1e40192
Matar Jacob <matar.j@outlook.com>
2023-03-20 18:50:59 +0200

    corsOptions add

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,26 @@ import { connectToDB } from "./connection";
 import dotenv from 'dotenv';
 const cors = require('cors');
 
+import allowedOrigins from '../config/allowedOrigins';
+
+const corsOptions = {
+    origin: (origin: string, callback: any) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200
+}
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT
 app.use(
-    cors(
-        //     {
-        //     origin: "https://matar-epicure.onrender.com/",
-        //     methods: ["GET, POST, DELETE"]
-        // }
-    )
+    cors(corsOptions)
 );
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
